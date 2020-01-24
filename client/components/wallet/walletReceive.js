50528b7c1941dd712833b7f43d930768ff528d9e
Robert McLeod <robert@datawallet.com>
2020-01-24 17:57:43 +0100

    add qr code

--- client/components/wallet/walletReceive.js
+++ client/components/wallet/walletReceive.js
@@ -1,4 +1,19 @@
+
 if (Meteor.isClient) {
+
+import QRCode from 'qrcode'
+
+  Template.walletReceive.onRendered(function() {
+    const addr = UserAccount.findOne().address
+    QRCode.toCanvas(addr, { errorCorrectionLevel: 'H' }, function (err, canvas) {
+      if (err) throw err
+    
+      var container = document.getElementById('qr-container')
+      container.appendChild(canvas)
+    })
+
+  })
+
   Template.walletReceive.helpers({
     account: function () {
       return UserAccount.findOne()
