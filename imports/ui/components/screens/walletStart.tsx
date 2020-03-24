71a1968d94acd89d532c3d4f5c498d143e146382
THORChain <accounts@thorchain.org>
2020-03-24 07:42:36 +0000

    Fix Wrong Images (#145)

--- imports/ui/components/screens/walletStart.tsx
+++ imports/ui/components/screens/walletStart.tsx
@@ -7,7 +7,7 @@ const StartScreen: React.FC = (): JSX.Element => {
     <div className="row">
 
       <div className="col-md-8 col-lg-6 ml-auto mr-auto">
-        <img className="mx-auto d-block w-25 mb-5" src="/img/Asgard-Wallet-Gradient-512.svg" />
+        <img className="mx-auto d-block w-25 mb-5" src="/img/Asgard-Tri-Gradient.svg" />
         <h5 className="text-center mb-5">Asgard Wallet</h5>
         <a className="form-control btn btn-dark btn-brand-border mb-4" href={FlowRouter.path('walletCreate')}>
           <span>New</span>
