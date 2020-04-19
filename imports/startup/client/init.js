5e0cf8300a674582eda07fbd069fcfac4bffd784
Mr. Smith <https://gitlab.com/a63ntsm1th>
2020-04-19 11:59:52 +0200

    minor fix

--- imports/startup/client/init.js
+++ imports/startup/client/init.js
@@ -2,6 +2,7 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import WalletController from '/imports/api/wallet';
 import '/imports/lib/schemas/schemasInit'
 
+
 export const WALLET = new WalletController();
 
 FlowRouter.wait();
